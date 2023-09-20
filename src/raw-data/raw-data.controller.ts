cbb2a7a15ba9bb382413c421ffac525ecc583f56
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-09-20 22:10:50 +0700

    upload controller and validation

--- src/raw-data/raw-data.controller.ts
+++ src/raw-data/raw-data.controller.ts
@@ -1,6 +1,8 @@
 import {
+  BadRequestException,
   Controller,
   Get,
+  InternalServerErrorException,
   Post,
   UploadedFile,
   UseInterceptors,
@@ -9,6 +11,7 @@ import { RawDataService } from './raw-data.service';
 import { RawData } from './schemas/raw-data.schema';
 import { FileInterceptor } from '@nestjs/platform-express';
 import * as Papa from 'papaparse';
+import * as path from 'path';
 
 @Controller('raw-data')
 export class RawDataController {
@@ -17,36 +20,88 @@ export class RawDataController {
   async getAllRawData(): Promise<RawData[]> {
     return this.rawDataService.findAll();
   }
-  // @Post()
-  // @UseInterceptors(FileInterceptor("file"))
-  // importUsers(@UploadedFile() file: Express.Multer.File) {
-  //   return this.rawDataService.importData(file.buffer.toString('base64')
-  // }
   @Post()
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FileInterceptor('files'))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
-    const results = [];
+    try {
+      if (path.extname(file.originalname) !== '.csv') {
+        throw new BadRequestException(
+          'File yang diinput harus ber-ekstensi .csv',
+          'FileExtensionError',
+        );
+      }
 
-    Papa.parse(file.buffer.toString(), {
-      header: true, // Jika baris pertama adalah header
-      dynamicTyping: true, // Otomatis mendeteksi tipe data
-      skipEmptyLines: true, // Melewati baris kosong
-      step: (result) => {
-        if (result.data) {
-          console.log(result.data);
-          // Handle data dari file CSV
-          // Pastikan data yang diunggah sesuai dengan model RawData
-          // const rawData = new this.rawDataModel(result.data);
-          // rawData.save(); // Simpan data ke dalam koleksi "raw_data"
-          // results.push(rawData);
-        }
-      },
-      complete: () => {
-        // Proses selesai
-        results.push({ message: 'Success' });
-      },
-    });
+      await new Promise<void>((resolve, reject) => {
+        Papa.parse(file.buffer.toString(), {
+          header: true,
+          dynamicTyping: true,
+          skipEmptyLines: true,
+          complete: async (result) => {
+            if (result.data && result.data.length > 0) {
+              for (const row of result.data) {
+                console.log(row);
+                let obj = {
+                  unique: '',
+                  resultTime: new Date(),
+                  enodebId: '',
+                  cellId: '',
+                  availDur: 0,
+                };
+                if (
+                  row['Object Name'] &&
+                  row['Result Time'] &&
+                  row['L.Cell.Avail.Dur']
+                ) {
+                  let pieces = row['Object Name'].split(',');
+                  let enodebIDPiece = pieces.find(function (piece: string) {
+                    return piece.includes('eNodeB ID');
+                  });
+                  let cellIDPiece = pieces.find(function (piece: string) {
+                    return piece.includes('Local Cell ID');
+                  });
 
-    return results;
+                  if (enodebIDPiece && cellIDPiece) {
+                    let enodebID = enodebIDPiece.split('=')[1];
+                    let cellID = cellIDPiece.split('=')[1];
+                    obj.unique =
+                      enodebID + '-' + cellID + '-' + row['Result Time'];
+                    obj.enodebId = enodebID;
+                    obj.cellId = cellID;
+                    obj.resultTime = new Date(row['Result Time']);
+                    obj.availDur = row['L.Cell.Avail.Dur'];
+                  } else {
+                    console.log(
+                      'Tidak ada eNodeB ID / result time / l.cell.avail.dur yang ditemukan.',
+                    );
+                  }
+
+                  const existingData = await this.rawDataService.findOne(
+                    obj.unique,
+                  );
+                  if (!existingData) {
+                    await this.rawDataService.create(obj);
+                  }
+                }
+              }
+            }
+
+            resolve();
+          },
+          error: (error: any) => {
+            console.error('Error parsing CSV:', error);
+            reject(
+              new InternalServerErrorException(
+                'Terjadi kesalahan dalam mengurai file CSV',
+                'CsvParsingError',
+              ),
+            );
+          },
+        });
+      });
+
+      return { message: 'Data dalam File berhasil ditambahkan ke database.' };
+    } catch (error) {
+      throw error;
+    }
   }
 }
